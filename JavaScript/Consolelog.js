beba6f104b960b18800b254c5d28196f35f16399
alokmotion <alokpatel406@gmail.com>
2024-01-23 21:12:57 +0530

    javascript console log

---
+++ JavaScript/Consolelog.js
@@ -0,0 +1,5 @@
+console.log("Namste JavaScript with double Quotes");
+console.log('Namste JavaScript with single quotes');
+// we can also print with backtick ``
+
+console.log(`Namste javaScript with Backtick`);
